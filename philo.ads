724461e9fc601b92267f89b7503eaa9224aa2e2d
Alexandre Tailleur <taille_a@epitech.eu>
2015-06-26 09:52:02 +0200

    remove useless record

--- philo.ads
+++ philo.ads
@@ -17,13 +17,6 @@ package philo is
    private
       Status : Status_St := down;
    end;
-
-   type Philosophe is record
-      Id : Integer := 0;
-      Time : Integer := 0;
-      Status : Status_T := Sleeping;
-   end record;
-
    task type Philosopher (Id : Philo; First, Second : not null access Stick);
    Sticks_e : array (1..8) of aliased Stick;
 end philo;
